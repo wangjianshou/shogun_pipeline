d2f9ff839842fe20a700ad22c2e15d59662fd6a6
wangjianshou <wangjianshou@ip-10-65-0-199.cn-northwest-1.compute.internal>
2019-01-06 04:23:56 +0000

    v1.0_2019-1-6

--- pool.py
+++ pool.py
@@ -4,9 +4,15 @@ import glob
 import multiprocessing
 
 def multiFun(tarBz2File):
-  os.system("python3 /k11e/pvdisk/fastbase/Users/wangjianshou/git/shogun_pipeline/Bz2TarFq_To_fa.py")
+  ofile = os.path.basename(tarBz2File).replace('.tar.bz2', '.fa')
+  outfile = os.path.join(sys.argv[2], ofile)
+  status = os.system("python3 /k11e/pvdisk/fastbase/Users/wangjianshou/git/shogun_pipeline/Bz2TarFq_To_fa.py " + tarBz2File + ' ' + outfile)
+  
 
-n_cpu = int(sys.argv[2])
+files = glob.glob(sys.argv[1])
+n_cpu = int(sys.argv[3])
 pool = multiprocessing.Pool(n_cpu)
 for f in files:
-  pool.apply_async(multiFun, args=(m, 
+  pool.apply_async(multiFun, args=(f,))
+pool.close()
+pool.join()
